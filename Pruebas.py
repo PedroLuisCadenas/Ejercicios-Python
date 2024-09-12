f1069878c6042752a953f1bb7f3eb9ead5433a0c
Pedro Luis <pedroluischute.1@gmail.com>
2024-09-12 10:44:15 +0200

    Función pirámide de asteriscos terminada

--- Pruebas.py
+++ Pruebas.py
@@ -1,14 +1,18 @@
 def piramide_asteriscos(numero):
-    for i in range(0, numero):
-        if (i == 0):
-            caracter = "*" * (i+1)
-            print(caracter)
-        else:
-            caracter = "*" * (i-1) 
-            print(caracter)    
-
+    
+    for i in range(1, numero+1):
+        if i % 2 == 1:
+            if i == 1 :
+                print(" "*(int(numero/2)) + "*"*i + " "*(int(numero/2)))
+            elif i > 1 and i < numero-1:
+                print(" "*(int((numero-i)/2)) + "*"*i + " "*(int((numero-i)/2)))
+        
+            else:
+                print("*"*(i))
+        else:   
+            continue
 
 
 #Prueba
-num = 5
+num = 15
 piramide_asteriscos(num)
